19d1482a4090f41b886f8abf29e5a0a98931e588
emtainfotech <emtainfotech@gmail.com>
2025-03-05 15:42:48 +0530

    screen checking

--- App/routing.py
+++ App/routing.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from VMS.consumers import MyWebSocketConsumer  # Import your WebSocket consumer
+from App.consumers import MyWebSocketConsumer  # Import your WebSocket consumer
 
 websocket_urlpatterns = [
     path("ws/screen/<str:emp_id>/", MyWebSocketConsumer.as_asgi()),
--- VMS/asgi.py
+++ VMS/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
-from VMS.routing import websocket_urlpatterns  # Import your WebSocket routes
+from App.routing import websocket_urlpatterns  # Import your WebSocket routes
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VMS.settings")
 
