54889458bbfaa5eeb4a20f74a9bd46f1d9733706
emtainfotech <info@emtainfotech.com>
2025-12-02 15:20:06 +0530

    database changes

--- VMS/settings.py
+++ VMS/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'VMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'emtavms_db',
+        'USER': 'EmtaDataBase',
+        'PASSWORD': 'Emta@0505',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
